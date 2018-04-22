877ea9cd64bd2f08a7b5efa91430ca9561b38b27
Maciej <moledzki@users.noreply.github.com>
2018-04-22 22:47:01 +0200

    refactor

--- postgres/run.sh
+++ postgres/run.sh
@@ -28,7 +28,8 @@ fi
 
 if [ ! -d $POSTGRESQL_DATA ]; then
     mkdir -p $POSTGRESQL_DATA
-    chown -R postgres:postgres /var/lib/postgresql
+    touch $NOT_READY_LOCK
+    chown -R postgres:postgres /var/lib/postgresql "
     sudo -u postgres /usr/lib/postgresql/9.3/bin/initdb -D $POSTGRESQL_DATA
     ln -s /etc/ssl/certs/ssl-cert-snakeoil.pem $POSTGRESQL_DATA/server.crt
     ln -s /etc/ssl/private/ssl-cert-snakeoil.key $POSTGRESQL_DATA/server.key
